341f93f2c87b8fa99c363705f5b6a0e722ce25a6
Sha674 <147704519+Sha674@users.noreply.github.com>
2025-11-10 13:28:20 +1100

    New Background View and edited medicine view

---
+++ Linc/Features/Handbook/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Linc
+//
+//  Created by Suenn Wern Tan on 10/11/2025.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
